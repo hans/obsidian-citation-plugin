1b43cd2d0250e3f68572f5317de02623e1a79646
Jon Gauthier <jon@gauthiers.net>
2021-01-10 11:05:00 -0500

    replace native fs.watch with chokidar

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
+import replace from '@rollup/plugin-replace';
 import webWorkerLoader from 'rollup-plugin-web-worker-loader';
 
 export default {
@@ -12,8 +13,22 @@ export default {
     format: 'cjs',
     exports: 'default',
   },
-  external: ['obsidian', 'path', 'fs', 'original-fs'],
+  external: ['obsidian', 'path', 'fs', 'util', 'events', 'stream', 'os'],
   plugins: [
+    /**
+     * Chokidar hacks to get working with platform-general Electron build.
+     *
+     * HACK: Manually replace fsevents import. This is only available on OS X,
+     * and we need to make a platform-general build here.
+     */
+    replace({
+      delimiters: ['', ''],
+      include: "node_modules/chokidar/**/*.js",
+
+      "require('fsevents')": "null",
+      "require('fs')": "require('original-fs')",
+    }),
+
     typescript(),
     nodeResolve({ browser: true }),
     commonjs({ ignore: ['original-fs'] }),
