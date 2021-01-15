25548dec4481a7e65530cc8e689a6a048b21b933
Jon Gauthier <jon@gauthiers.net>
2021-01-14 21:42:12 -0500

    view updates -- simple skeleton sort of working!

--- src/view.ts
+++ src/view.ts
@@ -1,11 +1,8 @@
-import { App, FileView, ItemView, TFile, View, WorkspaceLeaf } from "obsidian";
-import CitationPlugin from "./main";
-
+import { App, FileView, ItemView, TFile, View, WorkspaceLeaf } from 'obsidian';
+import CitationPlugin from './main';
 
 export class CitationsView extends ItemView {
-
   // Leaf corresponding to note file
-  private referenceLeaf: WorkspaceLeaf;
   private file?: TFile;
 
   private plugin: CitationPlugin;
@@ -14,51 +11,81 @@ export class CitationsView extends ItemView {
     super(leaf);
 
     this.plugin = plugin;
-    console.log("parent", this);
-    this.containerEl.createEl("p", {text: "This is a test"});
+    console.log('parent', this);
+    this.containerEl.createEl('p', { text: 'This is a test' });
+  }
+
+  load() {
+    this.registerEvent(
+      this.app.workspace.on('file-open', this.onFileOpen, this),
+    );
+    this.registerEvent(
+      this.app.vault.on('create', this.onFileChanged, this),
+    );
+    this.registerEvent(
+      this.app.vault.on('modify', this.onFileChanged, this),
+    );
+    this.registerEvent(
+      this.app.vault.on('rename', this.onFileRename, this),
+    );
+    this.registerEvent(
+      this.app.vault.on('delete', this.onFileDeleted, this),
+    );
+
+    console.log('citation view loaded');
   }
 
   async onOpen() {
     const activeLeaf = this.app.workspace.activeLeaf;
-    const activeFile = (activeLeaf?.view as FileView).file
+    const activeFile = (activeLeaf?.view as FileView).file;
 
     if (activeFile == null) {
       // TODO will this ever happen?
     }
 
-    this.referenceLeaf = activeLeaf;
     this.file = activeFile;
 
-    console.log("referenceLeaf", activeLeaf);
-
-    this.redraw();
+    // this.redraw();
   }
 
-  getCitations(): string[] {
-    console.log("my plugin", this);
-    console.log(this.plugin.editor);
-    const editorContent = this.plugin.editor?.getValue();
-    if (!editorContent) { return []; }
+  async getCitations(): Promise<string[]> {
+    const content = await this.app.vault.cachedRead(this.file);
 
     // TODO support other citation formats
-    let match = editorContent.matchAll(/\[\[@([^\]]+)\]\]/g),
-        matches = [...match];
-    console.log("match", matches);
-    return [];
+    let match = content.matchAll(/\[\[@([^\]]+)\]\]/g),
+      matches = [...match];
+    console.log('match', matches);
+    return matches.map((m) => m[1]);
   }
 
   async redraw() {
-    let citations = this.getCitations();
+    let citations = await this.getCitations();
+
+    const citationsUl = createEl('ul');
+    citations.forEach((c) => citationsUl.append(createEl('li', {text: c})));
+
+    this.containerEl.empty();
+    this.containerEl.append(citationsUl);
   }
 
   getViewType(): string {
-    return "citations";
+    return 'citations';
   }
 
   getDisplayText(): string {
-    console.log("state", this.getState());
-    // TODO how to get title of associated file?
-    return "Citations";
+    return 'Citations';
   }
 
+  onFileOpen(file: TFile) {
+    this.file = file;
+    this.redraw();
+  }
+
+  onFileChanged(file: TFile) {
+    this.redraw();
+  }
+
+  onFileRename(file: TFile) {}
+
+  onFileDeleted(file: TFile) {}
 }
